f77c52b2f684e98738f13ece93d20c9735c6190b
Anupam Singh <96816312+anupam-singh-rajput@users.noreply.github.com>
2024-04-29 12:49:34 -0700

    Pushing the project directory

---
+++ Context/contants.js
@@ -0,0 +1,11 @@
+import crowdFunding from "./CrowdFunding.json";
+
+//NFT MARKETPLACE
+//LOCAL HOST TESTING HARDHAT
+// export const CrowdFundingAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+// POLYGON MUMBAI
+// 0x707755bdf12cad7e7318065eff5e434f3264a300
+// 0xa6E565fa588c9472353D70CE0f2BCCA6b9810885
+export const CrowdFundingAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+export const CrowdFundingABI = crowdFunding.abi;
